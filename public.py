adb5f239a1a856d1efd56a6ad4769f0e3a785f66
Luca <l.albertalli@gmail.com>
2026-07-09 19:33:22 -0700

    big refactor. Add argument parse and scheleton for rebalance.

--- public.py
+++ public.py
@@ -1,29 +1,11 @@
+import argparse
+
 from public_api_sdk import PublicApiClient, PublicApiClientConfiguration, ApiKeyAuthConfig
 from config import ALLOCATIONS, CHECK_ACCOUNTS
 from decimal import Decimal
 
-with open(".publicdotcom_key") as f:
-    key = f.readline()
 
-client = PublicApiClient(
-    ApiKeyAuthConfig(api_secret_key=key), 
-    config=PublicApiClientConfiguration()
-    )
-
-accounts = client.get_accounts()
-
-for acc in accounts.accounts:
-    print(acc.account_type.name, acc.account_id)
-
-def string_format(name, length = 40):
-    if len(name) > length:
-        return name[:length]
-    return name.ljust(length)
-
-def number_format(number, symbol, length = 12):
-    return ("%s %s"%(number, symbol)).rjust(length)
-
-FORMAT = [
+FORMAT_SHOW = [
     ("Name", 40, lambda x: string_format(x)),
     ("Symbol", 10, lambda x: string_format(x, 10)),
     ("Value", 12, lambda x: number_format(x, "$")),
@@ -33,33 +15,50 @@ FORMAT = [
     ("Gain/Loss", 12, lambda x: number_format(x, "$"))
     ]
 
+
+FORMAT_REBALANCE = [
+    ("Name", 40, lambda x: string_format(x)),
+    ("Symbol", 10, lambda x: string_format(x, 10)),
+    ("Value", 12, lambda x: number_format(x, "$")),
+    ("Buy Sell", 15, lambda x: number_format(x, "%", 15)),
+    ("Amount", 12, lambda x: number_format(x, "%")),
+    ("New Balance", 12, lambda x: number_format(x, "$")),
+    ]
+
+
+# Formatter helper functions
+FORMAT = FORMAT_SHOW
+def string_format(name, length = 40):
+    if len(name) > length:
+        return name[:length]
+    return name.ljust(length)
+
+
+def number_format(number, symbol, length = 12):
+    return ("%s %s"%(number, symbol)).rjust(length)
+
+
 def print_divider():
     div_len = sum(i for _,i,_ in FORMAT) + 3 * (len(FORMAT)-1) + 4
     print("-"* div_len)
 
+
 def print_header(account):
-    print("\n Portfolio %s\n"%account)
+    print("\n\n Portfolio %s\n"%account)
     print_divider()
     print("| "+ " | ".join([string_format(i,j) for i,j,_ in FORMAT]) + " |")
     print_divider()
 
+
 def print_row(row):
     print("| " + " | ".join(f(r) for r,(_,_,f) in zip(row,FORMAT)) + " |")
 
-for k,v in CHECK_ACCOUNTS.items():
-    portfolio = client.get_portfolio(v)
-    value = portfolio.equity[0].value
-    cash = portfolio.buying_power.cash_only_buying_power
-    positions = portfolio.positions
-    total_val = Decimal(0.0)
-    total_cb = Decimal(0.0)
-    total_pct = Decimal(0.0)
-    abs_delta = Decimal(0.0)
-    total_change_b = Decimal(0.0)
-    print_header(k)
-    allocations = ALLOCATIONS.get(k,ALLOCATIONS[None])
+
+def portfolio_allocation_analysis(positions,allocations):
+    shown_symbols = []
     for p in positions:
         symbol = p.instrument.symbol
+        shown_symbols += [symbol]
         name = p.instrument.name
         cost_basis = p.cost_basis.total_cost
         current_value = p.current_value
@@ -68,16 +67,161 @@ for k,v in CHECK_ACCOUNTS.items():
             a = allocations[symbol]["allocation"]
         except KeyError:
             a = Decimal(0.0)
-        delta = percentage - a
         change_from_basis = current_value - cost_basis
-        print_row([name, symbol,current_value, percentage, 
-            delta, cost_basis, change_from_basis])
+        yield [name, symbol,current_value, percentage, 
+            a, cost_basis, change_from_basis]
+
+    for symbol, a in allocations.items():
+        if symbol not in shown_symbols:
+            shown_symbols+=[symbol]
+            yield ["", symbol, Decimal(0.0), Decimal(0.0), 
+                a['allocation'], Decimal(0.0), Decimal(0.0)]
+
+
+def get_target_allocation(name):
+    return ALLOCATIONS.get(name,ALLOCATIONS[None])
+
+
+
+def parse_portfolio(portfolio):
+    return (
+        sum(i.value for i in portfolio.equity), 
+        [i for i in portfolio.equity if i.type == 'CASH'][0],
+        portfolio.positions
+        )
+
+
+def print_account_info(portfolio, name):
+    allocations = get_target_allocation(name)
+    value, cash, positions = parse_portfolio(portfolio)
+
+    total_val = Decimal(0.0)
+    total_cb = Decimal(0.0)
+    total_pct = Decimal(0.0)
+    abs_delta = Decimal(0.0)
+    total_change_b = Decimal(0.0)
+
+    print_header(name)
+    shown_symbols = []
+    for r in portfolio_allocation_analysis(positions, allocations):
+        name, symbol,current_value, percentage, alloc, cost_basis, change_from_basis = r
+        delta = alloc - percentage
+        print_row([name, symbol,current_value, percentage, delta, 
+            cost_basis, change_from_basis])
         total_val += current_value
         total_cb += cost_basis
         total_pct += percentage
         abs_delta += abs(delta)
         total_change_b += change_from_basis
+
     print_divider()
+    print_row(["Total stock", "", total_val, total_pct, 
+            abs_delta, total_cb, total_change_b])
+    print_divider()
+    print_row(["Cash", "", cash.value, cash.percentage_of_portfolio, 
+        cash.percentage_of_portfolio, 0.0, 0.0])
+    print_divider()
+    total_val += cash.value
+    total_pct += cash.percentage_of_portfolio
+    abs_delta += abs(cash.percentage_of_portfolio)
     print_row(["Total", "", total_val, total_pct, 
             abs_delta, total_cb, total_change_b])
     print_divider()
+
+    # suggest rebalance
+    cash_reb = cash.value > Decimal(20.0)
+    abs_delts_reb = abs_delta > len(allocations) * Decimal(0.15)
+    if abs_delts_reb or cash_reb:
+        print("Suggested to rebalance this portfolio. Causes:")
+        if abs_delts_reb:
+            print("- Portfolio is out of balance of %.2f%%. Max is %.2f%%"%(
+                abs_delta, len(allocations) * Decimal(0.15)))
+        if cash_reb:
+            print("- Portfolio has excessive cash balance %.2f$. Max is 20$"%(cash.value))
+
+
+def calculate_rebalance(portfolio, name):
+    allocations = get_target_allocation(name)
+    value, cash, positions = parse_portfolio(portfolio)
+    # WIP
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Help me monitor my portfolio on Public.com and keep it balanced',
+        )
+    parser.add_argument("action", choices=['show', 'rebalance'], 
+        default = 'show', 
+        help = "Action to be execute:\n\
+    - show: Show the current portfolio\n\
+    - rebalance: Rebalance the portfolio. Notice, without --run,\
+ it will only simulate the rebalance",
+        nargs = '?')
+    parser.add_argument("--run", action = 'store_true',
+        help = "For rebalance, actually use the public apis to run the planned actions")
+
+    parser.add_argument('--account', 
+        help = "Limit to the specified account")
+
+    return parser.parse_args()
+
+
+def show(client, account):
+    for k,v in CHECK_ACCOUNTS.items():
+        if not account or k == account:
+            portfolio = client.get_portfolio(v)
+            print_account_info(portfolio, k)
+
+
+def rebalance(client, account, run):
+    for k,v in CHECK_ACCOUNTS.items():
+        if not account or k == account:
+            portfolio = client.get_portfolio(v)
+            
+
+
+def validate_allocations(allocations):
+    error = False
+    for name,allocs in allocations.items():
+        total_pct = Decimal(0.0)
+        for symbol, a in allocs.items():
+            try:
+                total_pct += Decimal(a["allocation"])
+            except:
+                pass
+            if type(a["allocation"]) != Decimal:
+                error = True
+                print(
+                    "Error validating the configuration for %s. \
+The allocation should be of type Decimal. '%s' is not" % ((name or "Default (None)"), symbol))
+        if total_pct != Decimal(100.0):
+            error = True
+            print("Error validating the configuration for %s. \
+Total allocation should be 100%%. Found %f" % ((name or "Default (None)"), total_pct))
+    return not error
+
+
+def main():
+    args = parse_args()
+    
+    try:
+        with open(".publicdotcom_key") as f:
+            key = f.readline()
+    except:
+        print("Error loading the key for public.com from the file .publicdotcom_key")
+        return
+
+    if not validate_allocations(ALLOCATIONS):
+        return
+
+    client = PublicApiClient(
+        ApiKeyAuthConfig(api_secret_key=key), 
+        config=PublicApiClientConfiguration()
+        )
+    if args.action == "show":
+        show(client, args.account)
+    elif args.action == "rebalance":
+        rebalance(client, args.account, args.run)
+
+if __name__ == "__main__":
+    main()
